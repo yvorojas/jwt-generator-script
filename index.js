1051c19d028914619d57e948a7731bb7f83e577b
Yvo Rojas <yvo.rojas.valdes@gmail.com>
2018-09-24 23:45:28 -0300

    refactor consts
    refactor payload

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-// Const definition
-const SH_ROUTE = './generate_certs.sh';
-const CERTS_FOLDER_ROUTE = './certs';
-const PRIVATE_CERT_ROUTE = './certs/private_cert.pem';
-const PUBLIC_CERT_ROUTE = './certs/public_cert.pub';
-const TOKEN_ROUTE = 'token'; 
-const RSA_FILE_ROUTE = './certs/private_cert.pem.pub';
+// consts dependency
+const consts = require('./consts');
 
 // File control dependency
 const fs = require('fs');
 
 // Delete previous generated token
-if (fs.existsSync(TOKEN_ROUTE)){
-    fs.unlinkSync(TOKEN_ROUTE);
+if (fs.existsSync(consts.TOKEN_ROUTE)){
+    fs.unlinkSync(consts.TOKEN_ROUTE);
 }
 
 // Delete previous certs files
-const existsCertFolder = fs.existsSync(CERTS_FOLDER_ROUTE);
+const existsCertFolder = fs.existsSync(consts.CERTS_FOLDER_ROUTE);
 if (existsCertFolder) {
-    fs.readdirSync(CERTS_FOLDER_ROUTE).forEach(file => {
-        fs.unlinkSync(`${CERTS_FOLDER_ROUTE}/${file}`);
+    fs.readdirSync(consts.CERTS_FOLDER_ROUTE).forEach(file => {
+        fs.unlinkSync(`${consts.CERTS_FOLDER_ROUTE}/${file}`);
     });
 } else {
-    fs.mkdirSync(CERTS_FOLDER_ROUTE);
+    fs.mkdirSync(consts.CERTS_FOLDER_ROUTE);
 }
 
 // Dependency to execute shell script to create priv and pub cert 
@@ -36,46 +31,51 @@ const shelljs = require('shelljs');
 // Execute sh
 console.log('-----------------------------------------------------------');
 console.log('GENERATING CERTS...');
-shelljs.exec(SH_ROUTE, {silent:true});
+shelljs.exec(consts.SH_ROUTE, {silent:true});
 console.log('CERTS GENERATED SUCCESSFULLY!!!');
 
-fs.unlinkSync(RSA_FILE_ROUTE);
+fs.unlinkSync(consts.RSA_FILE_ROUTE);
 
 // JWT dependencies
 const jwt = require('express-jwt');
 const jsonwebtoken = require('jsonwebtoken');
 
-//route for private cert
-const privCert = fs.readFileSync(PRIVATE_CERT_ROUTE); 
+// Route for private cert
+const privCert = fs.readFileSync(consts.PRIVATE_CERT_ROUTE);
 
-//function to generate a jwt based on privCert with RS256 Algorithm and a duraction of 2 years, with a assigned payload
-var token = jsonwebtoken.sign(
-    { 
-        type: 'API_KEY',
-        application_name: 'API_COTIZACION',
-        environment: 'DEVELOPMENT',
-    }, privCert, 
-    { 
-        algorithm: 'RS256',
-        expiresIn: '730d',
-    });
+// JWT Payload Definition, a simple object
+const payload = {
+    message: 'I AM A PAYLOAD!',
+    otherProperty: 'Value',
+    environment: 'DEVELOPMENT',
+    application_name: 'SAMPLE',
+};
 
-//save token in file
+// JWT algorithm object definition.
+const algorithm =  { 
+    algorithm: 'RS256',
+    expiresIn: '730d',
+}
+
+// Function to generate a JWT based on privCert with RS256 Algorithm and a duraction of 2 years, with a assigned payload
+const token = jsonwebtoken.sign(payload, privCert, algorithm);
+
+// Save JWT token in file
 console.log('-----------------------------------------------------------');
 console.log('SAVE TOKEN FILE...');
-fs.writeFileSync(TOKEN_ROUTE, token);
+fs.writeFileSync(consts.TOKEN_ROUTE, token);
 console.log('TOKEN FILE GENERATED SUCCESSFULLY!!');
 
-//route for private cert
-var pubCert = fs.readFileSync(PUBLIC_CERT_ROUTE);  // route for public cert
+// Route for private cert
+const pubCert = fs.readFileSync(consts.PUBLIC_CERT_ROUTE);  // route for public cert
 
-//Verify private token with pubCert, and obtain the decoded token
-jsonwebtoken.verify(token, pubCert, function(err, decoded) {
-  //If error, show error in console
+// Verify private token with pubCert, and obtain the decoded token
+jsonwebtoken.verify(token, pubCert, (err, decoded) => {
+// If error, show error in console
   if (err){
       console.log(err);
   }
-  //show decode token in console
+// Show decode token in console
   console.log('-----------------------------------------------------------');
   console.log('DECODED TOKEN');
   console.log(decoded)
